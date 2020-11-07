5a56db8b1a2b13f708c895c1ee69c01c16f5a566
mitlg7 <mitlg7@gmail.com>
2020-11-07 23:28:32 +0400

    Промежуточный результат. ►Настоен роутинг ►Понят и создана первая FormData в ангуляре ►разобрался http отправкой запросов в ангуляре

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdCardComponent } from './ad-card/ad-card.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { AdPageComponent } from './ad-page/ad-page.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CreateAdComponent } from './create-ad/create-ad.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'ad/:id', component: AdPageComponent},
+  {path: 'user/:id', component: UserPageComponent},
+  {path: 'create', component: CreateAdComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AdCardComponent,
+    UserPageComponent,
+    AdPageComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CreateAdComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
